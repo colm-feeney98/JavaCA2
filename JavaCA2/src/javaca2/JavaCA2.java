5b9da86888e225de3f4cc84cc2fea4d16edec9e6
colm-feeney <sba24002@student.cct.ie>
2024-12-14 16:34:50 +0000

    Initial push
    Quick initial code to push

--- JavaCA2/src/javaca2/JavaCA2.java
+++ JavaCA2/src/javaca2/JavaCA2.java
@@ -4,17 +4,33 @@
  */
 package javaca2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author colmj
  */
 public class JavaCA2 {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //sc is object of scanner class
+        Scanner sc=new Scanner(System.in);
+        
+        //Input size of the square matrix
+         System.out.print("Enter size of the coloum matrix: ");
+        int row=sc.nextInt();
+        
+        System.out.print("Enter size of the coloum matrix: ");
+        int col=sc.nextInt();
+        
+        //intilaze the square matrix
+        int[][] matrix = new int[row][col];
     }
     
 }
