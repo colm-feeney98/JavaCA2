e29e77fd47526928765ecfb80597c68a399c639c
colm-feeney <sba24002@student.cct.ie>
2024-12-15 17:57:12 +0000

    Array Search
    Completed code for array search

--- JavaCA2/src/javaca2/JavaCA2.java
+++ JavaCA2/src/javaca2/JavaCA2.java
@@ -4,6 +4,7 @@
  */
 package javaca2;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -19,18 +20,53 @@ public class JavaCA2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //sc is object of scanner class
-        Scanner sc=new Scanner(System.in);
-        
-        //Input size of the square matrix
-         System.out.print("Enter size of the coloum matrix: ");
-        int row=sc.nextInt();
-        
-        System.out.print("Enter size of the coloum matrix: ");
-        int col=sc.nextInt();
-        
-        //intilaze the square matrix
-        int[][] matrix = new int[row][col];
+         Scanner scanner = new Scanner(System.in);
+
+        // Take the size of the array as input
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // Handle edge cases for invalid sizes
+        if (size <= 0) {
+            System.out.println("Invalid array size.");
+            return;
+        }
+
+        int[] array = new int[size];
+
+        // Take the elements of the array as input
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Find the first repeated element
+        int firstRepeated = findFirstRepeated(array);
+
+        // Output the result
+        if (firstRepeated != -1) {
+            System.out.println("First repeated element: " + firstRepeated);
+        } else {
+            System.out.println("No repeated elements found.");
+        }
+
+        scanner.close();
     }
-    
+
+    private static int findFirstRepeated(int[] array) {
+        // Use a HashSet to track seen elements
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : array) {
+            // If the element is already in the set, it's the first repeated
+            if (seen.contains(num)) {
+                return num;
+            }
+            // Otherwise, add the element to the set
+            seen.add(num);
+        }
+
+        // Return -1 if no repeated elements are found
+        return -1;
+    }
 }
